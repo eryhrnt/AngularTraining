3376dccbe8feb87509739d10a4531b121645d91c
Ery Harinanto <eryharinanto111@gmail.com>
2024-09-17 13:31:12 +0700

    update training

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,
+  imports: [CommonModule,
+            RouterOutlet,
             NavbarComponent,
             CourseComponent],
   templateUrl: './app.component.html',
@@ -15,5 +17,24 @@ import { CoursesService } from './service';
   providers: [CoursesService]
 })
 export class AppComponent {
-  title = 'Ery Harinanto';
+  courses = [
+    {id: 1, name: "course1"},
+    {id: 2, name: "course2"},
+    {id: 3, name: "course3"}
+  ];
+
+  onAdd(){
+    this.courses.push({id: 4, name: "course4"});
+  }
+
+  onRemove(course: { id: number; name: string; }){
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+  onChange(course: { id: number; name: string; }){
+    course.name = 'UPDATED';
+
+  }
+
+  viewMode = "somethingElse";
 }
